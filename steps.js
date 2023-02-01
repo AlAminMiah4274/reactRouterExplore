22ca54fe8a88b15044a2a882c70b8749a6a8834a
AlAminMiah4274 <alaminmiah4274@gmail.com>
2023-02-01 20:33:06 +0600

    created products component

--- steps.js
+++ steps.js
@@ -4,3 +4,10 @@
 3. add RouterProvider and pass router props
 4. create some routes
 */
+
+/*
+Things you need to creat a route:
+1. Create a link: so that you can go this route
+2. Create a component: to add that you will show once you go to that route
+3. Add route so that react router know the component it needs to display while you are visiting to that route.
+*/
