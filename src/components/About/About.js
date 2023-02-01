9e90bb6014588630907298e9779263cbe92049fb
AlAminMiah4274 <alaminmiah4274@gmail.com>
2023-02-01 20:19:52 +0600

    created header component and declared link

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Header from '../Header/Header';
 
 const About = () => {
     return (
         <div>
+            <Header></Header>
             <h3>This is the truth real about me page</h3>
         </div>
     );
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Header from '../Header/Header';
 
 const Home = () => {
     return (
         <div>
+            <Header></Header>
             <h3>This is home component</h3>
         </div>
     );
