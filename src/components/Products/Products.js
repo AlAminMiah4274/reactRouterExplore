d1eaa8bacfaeb871fef60fceb4439516ca2a2f29
AlAminMiah4274 <alaminmiah4274@gmail.com>
2023-02-01 21:37:49 +0600

    added style in header

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Header from '../Header/Header';
 
 const Products = () => {
     return (
         <div>
+            <Header></Header>
             <h3>This is product component</h3>
         </div>
     );
