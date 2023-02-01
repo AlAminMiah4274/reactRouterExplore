28426c4e4a1e8e273353af4e2023ab45d9f35a2f
AlAminMiah4274 <alaminmiah4274@gmail.com>
2023-02-01 19:24:26 +0600

    created router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import logo from './logo.svg';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
 
 function App() {
+  const router = createBrowserRouter([
+    { path: '/', element: <div>This is default page</div> },
+    { path: '/home', element: <div>Home Page</div> },
+    { path: 'about', element: <div>This is about page</div> }
+  ]);
   return (
     <div className="App">
-
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
